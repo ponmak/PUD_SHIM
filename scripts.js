ccb879011cf5dbac4c6eb6a41468c206c815bc9d
ponmak <peeranut.4498@gmail.com>
2024-02-19 00:29:44 +0700

    ku

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const url_link = ["http://www.reddit.com","https://www.youtube.com/watch?v=VxR_BYPG7v4"]
+const url_link = ["https://drive.google.com/file/d/1ouk_NP57ikbor9b4RLhUKLP9BjcP1yzB/view?usp=sharing","https://drive.google.com/file/d/1Qisy6oV7RmgpH-IRxrFzUt47dhMFjWln/view?usp=sharing","https://drive.google.com/file/d/1ZpC9r977H1mHK8-Jun_mQJSiNCUEQRXX/view?usp=sharing","https://drive.google.com/file/d/1YJLBHlr1ZV2bZOTzM-t4xEDWu5z5qpn_/view?usp=sharing","https://drive.google.com/file/d/1T2pbWGcEhxZGUzEVPf9ZFe3eKC8EBQBI/view?usp=sharing"]
 
 const rand = (items) => {
     return items[items.length * Math.random() | 0];
